a4a14402a04ed4fdf1b8b1e55246dcc84068ac22
Nithish S <128178765+nithish-95@users.noreply.github.com>
2024-05-10 12:50:10 -0700

    made some progress

---
+++ src/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Twitter Analysis")
+
+	consumerKey := "iedOnr3VrHywZcbASmNUXSf5v"
+	consumerSecret := "AfxnFPVX2CpLAvX0pyhwo9iLu4bt4mTK374IsQ1eMeX7eTX5L0"
+	accessToken := "1110529818406248448-2wRVrEjsLSHvjKuEBgaTpDudcdVcRD"
+	accessSecret := "wXGiaVjr2y4Vw2x7Im2VtxjM8lCrCPqhhZZtuAT0bVqKM"
+}
